a1ec7b4abbe8417d77cc1dd2f35e68aadec84aa1
mgasperini <matiasgasperini@gmail.com>
2022-11-03 00:21:36 +0100

    Fixed: INSERT queries agregadas en variables

--- WorldCup/insert_data.sh
+++ WorldCup/insert_data.sh
@@ -27,19 +27,17 @@ do
   # Insert Winner if not exists
   if [[ -z $WINNER_ID ]]
   then
-    $($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")
+    INSERT_WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER';")
   fi
   # Insert opponent if not exists
   if [[ -z $OPPONENT_ID ]]
   then
-   $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');")
+    INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');")
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT';")
   fi
 
-
-
-$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")
+INSERT_GAME=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")
 
 
 done
